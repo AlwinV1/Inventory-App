6c86b8826091f9b07cd6da3cb9747de44168ad40
AlwinV1 <91858784+AlwinV1@users.noreply.github.com>
2022-06-06 16:42:47 -0400

    Add files via upload

---
+++ github.com/whitehatjr/e-library-v2-pro-c70/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyO982nkXlgvCMUnA30GAnThTlXphFPWY",
+  authDomain: "e-library-app-273bf.firebaseapp.com",
+  projectId: "e-library-app-273bf",
+  storageBucket: "e-library-app-273bf.appspot.com",
+  messagingSenderId: "1093141544330",
+  appId: "1:1093141544330:web:786dfc818ba28ff8d6dc73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
